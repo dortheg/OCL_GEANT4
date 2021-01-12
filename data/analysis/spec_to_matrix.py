46be570bced6315b2e0f6e37da4a4ab77a0a763a
Dorthea <dorthea.gjestvang@gmail.com>
2021-01-12 14:50:03 +0100

    spec_to_matrix runs

--- data/analysis/spec_to_matrix.py
+++ data/analysis/spec_to_matrix.py
@@ -25,23 +25,19 @@ def get_counts_bgsubtract(vec, energy, extend=10, ext_bg=None):
         extend: get area for energy+-extend, as
             counts are often placed in some bins around the "sharp" energy
         ext_bg: extend where bg should be taken into account
-
-    Returns:
-        peak_wo_bg, bg_avg: Peak with out background, and background
     """
 
     if ext_bg is None:
-        ext_bg = extend+4
+        ext_bg = extend+5
 
-    bg_below = get_area(vec, energy-ext_bg, energy-extend)
+    bg_below = get_area(vec, energy-ext_bg, energy-ext_bg+extend)
     bg_above = get_area(vec, energy+extend,  energy+ext_bg)
-    # note that bg is summed one additional channel
-    bg_avg = (bg_below + bg_above) / (2*(ext_bg-extend+1))
+    bg_avg = (bg_below + bg_above) / (2*(ext_bg-extend))
 
     peak_counts = get_area(vec, energy-extend, energy+extend)
     peak_wo_bg = peak_counts - bg_avg*extend
 
-    return peak_wo_bg, bg_avg
+    return peak_wo_bg
 
 
 def get_efficiencies(vec, energy):
@@ -52,43 +48,29 @@ def get_efficiencies(vec, energy):
         energy: Energy of the FE peak
 
     """
-    mama_vec = vec.copy()
     df = pd.Series()
     df["E"] = energy
-    df["tot"] = vec.values.sum() / nevent
     df["tot>50keV"] = vec.values[vec.index(50):].sum() / nevent
     df["tot>200keV"] = vec.values[vec.index(200):].sum() / nevent
     df["tot>500keV"] = vec.values[vec.index(500):].sum() / nevent
 
-    eff_photo, bg = get_counts_bgsubtract(vec, energy, extend=10) / nevent
-    replace_counts(mama_vec, energy-5, energy+5, bg*nevent)
+    eff_photo = get_counts_bgsubtract(vec, energy, extend=10) / nevent
     df["fe"] = eff_photo
 
     if energy > 2*511:
-        eff_, bg = get_counts_bgsubtract(vec, energy-511, extend=5) / nevent
-        e = energy-511
-        replace_counts(mama_vec, e-5, e+5, bg*nevent)
+        eff_ = get_counts_bgsubtract(vec, energy-511, extend=10) / nevent
         df["se"] = eff_
 
-        eff_, bg = get_counts_bgsubtract(vec, energy-511*2, extend=5) / nevent
-        e = energy-511*2
-        replace_counts(mama_vec, e-5, e+5, bg*nevent)
+        eff_ = get_counts_bgsubtract(vec, energy-511*2, extend=10) / nevent
         df["de"] = eff_
 
-        eff_, bg = get_counts_bgsubtract(vec, 511, extend=5) / nevent
-        e = 511
-        replace_counts(mama_vec, e-5, e+5, bg*nevent)
+        eff_ = get_counts_bgsubtract(vec, 511, extend=5) / nevent
         df["511"] = eff_
     else:
         df["se"] = 0
         df["de"] = 0
         df["511"] = 0
-    return df, mama_vec
-
-
-def replace_counts(vec, E1, E2, counts):
-    """ Replace counts in vec by new value """
-    vec[vec.index(E1):vec.index(E2)+1] = counts
+    return df
 
 
 def efficiency_plots(efficiencies: pd.DataFrame, energy_grid):
@@ -117,74 +99,6 @@ def efficiency_plots(efficiencies: pd.DataFrame, energy_grid):
     fig.tight_layout(pad=0.02)
     return fig, ax
 
-def export_Guttormsen1996_interpolation():
-    """ write files for Guttormsen1996 response interpolation
-
-    TODO: clean up / refactor
-    """
-    for i, (energy, nevent) in enumerate(zip(tqdm(energy_grid), nevents)):
-        fn = specdir / f"grid_{energy}keV_n{nevent}.root.m"
-        if not fn.exists():
-            continue
-
-        vec = om.Vector(path=fn, units="MeV")
-        vec.to_keV()
-
-        eff.loc[i], mama_vec = get_efficiencies(vec, eff["E"][i])
-
-        # rebin and smooth; rebin first to save time smoothing
-        vec.rebin(mids=energy_out_uncut)
-        vec.values = om.gauss_smoothing(vec.values, vec.E,
-                                        fFWHM(vec.E, fwhm_pars))
-        vec.rebin(mids=energy_out)
-
-        respmat.values[i, :] = vec.values
-
-        ompy_vec = mama_vec.copy()
-        ompy_vec.cut(Emax=energy + 2000)
-        ompy_vec.save(f"{fnompy}/cmp{energy}", filetype="mama", _assert=False)
-        if energy < 10000:
-            mama_vec.cut(Emax=10000)
-            mama_vec.rebin(factor=2)
-        else:
-            mama_vec.rebin(factor=3)
-
-        if energy != 50:
-            mama_vec.save(f"{fnmama}/cmp{energy}", filetype="mama")
-
-        eff_mama.loc[i] = eff.loc[i]
-        eff_mama.loc[i, ["fe", "se", "de", "511"]] *= nevent
-        # # get before smoothing, not after -> large effect
-        # # eff_photo = get_area(vec, energy-10, energy+10) / nevent
-        # # print(eff_photo)
-
-        # vec.values /= i  # scale number of events
-        # # vec.save(f"export/resp_m{i}.m")
-        # vec.plot(ax=ax)
-
-    eff.to_csv("efficiencies.csv")
-
-    eff_mama = eff_mama.iloc[1:]
-    eff_mama.drop(["tot>50keV", "tot>200keV", "tot>500keV"], axis=1,
-                  inplace=True)
-    fwhm_rel = fFWHM(eff_mama["E"], fwhm_pars) / eff_mama["E"]
-    idx = abs(eff_mama['E'] - 1333).idxmin()
-    fwhm_rel_1333 = fwhm_rel[idx]
-    eff_mama.insert(1, "FWHM", fwhm_rel/fwhm_rel_1333)
-
-    eff_mama["tot"] /= eff_mama["tot"][idx]
-    mama_header =(
-        "# OSCAR 2020 LaBr; 16.3 cm distance\n"
-        "# Response functions from article arXiv:2008.06240v2 | Response functions simulated by GEANT4 (Fabio and Gry, August 2020) || FWHM rel. and Eff Normalized to 1 at 1.33 MeV.\n"
-        "# Next: Numer of Lines\n"
-        f"{len(eff_mama)}\n"
-        "# Eg        FWHM_rel      Eff_tot       FE            SE            DE            c511"
-        )
-    for fdir in [fnmama, fnompy]:
-        np.savetxt(fdir / "resp.dat", eff_mama.to_numpy(), fmt='%.4e',
-                   delimiter="\t", header=mama_header, comments="")
-        print("saved mama")
-
 
 if __name__ == "__main__":
     figs_dir = Path("figs")
@@ -193,7 +107,6 @@ if __name__ == "__main__":
     response_outdir = Path("response_export")
     response_outdir.mkdir(exist_ok=True)
 
-    #Fabio energies used
     #energy_grid = np.arange(50, 1e4, 10, dtype=int)
     #nevents = np.linspace(6e5, 3e6, len(energy_grid), dtype=np.int)
 
@@ -204,10 +117,9 @@ if __name__ == "__main__":
     nevents = np.zeros(len(energy_grid))
     nevents.fill(100000)
 
-    #What is this?
+
     fwhm_pars = np.array([60.6499, 0.458252, 0.000265552])
 
-    #What is this?
     energy_out = np.arange(energy_grid[0], 21000, 10)
     energy_out_uncut = np.arange(0, 21000, 10)
     # response mat. with x: incident energy; y: outgoing
@@ -215,25 +127,45 @@ if __name__ == "__main__":
                         Ex=energy_grid,
                         Eg=energy_out)
 
-    eff = pd.DataFrame(columns=["E", "tot", "tot>50keV", "tot>200keV",
-                                "tot>500keV",
+    eff = pd.DataFrame(columns=["E", "tot>50keV", "tot>200keV", "tot>500keV",
                                 "fe", "se", "de", "511"])
     eff["E"] = energy_grid
 
-    #specdir = Path("from_geant")
     specdir = Path("mama_spectra")
-
     # fig, ax = plt.subplots()
     # fig_plain, ax_plain = plt.subplots()
 
-    fnmama = Path("mama_export")
-    fnmama.mkdir(exist_ok=True)
+    for i, (energy, nevent) in enumerate(zip(tqdm(energy_grid), nevents)):
 
-    fnompy = Path("ompy_interpolate")
-    fnompy.mkdir(exist_ok=True)
+        # if energy > 1000: # just for testing
+        #     break
 
-    # export files for interpolation with mama / ompy
-    export_Guttormsen1996_interpolation()
+        fn = specdir / f"grid_{energy}keV_n{nevent}.root.m"
+        if not fn.exists():
+            continue
+
+        vec = om.Vector(path=fn, units="MeV")
+        vec.to_keV()
+
+        eff.loc[i] = get_efficiencies(vec, eff["E"][i])
+
+        # rebin and smooth; rebin first to same time smoothing
+        vec.rebin(mids=energy_out_uncut)
+        vec.values = om.gauss_smoothing(vec.values, vec.E,
+                                        fFWHM(vec.E, fwhm_pars))
+        vec.rebin(mids=energy_out)
+
+        respmat.values[i, :] = vec.values
+
+        # # get before smoothing, not after -> large effect
+        # # eff_photo = get_area(vec, energy-10, energy+10) / nevent
+        # # print(eff_photo)
+
+        # vec.values /= i  # scale number of events
+        # # vec.save(f"export/resp_m{i}.m")
+        # vec.plot(ax=ax)
+
+    eff.to_csv("efficiencies.csv")
 
     # different version of response matrix
     fn = "response_unnorm"
